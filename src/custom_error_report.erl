39a5232bff4466aae0f70593e82220ac6839f440
Garrett Cradduck <gcradduck@gmail.com>
2015-01-11 16:11:28 -0600

    Completed gen_event / event logging.

---
+++ src/custom_error_report.erl
@@ -0,0 +1,36 @@
+-module(custom_error_report).
+
+-behaviour(gen_event).
+
+%% API
+-export([register_with_logger/0]).
+
+-export([
+	 init/1, handle_event/2, handle_call/2,
+	 handle_info/2, terminate/2, code_change/3
+	 ]).
+
+-record(state, {}).
+
+register_with_logger() ->
+    error_logger:add_report_handler(?MODULE).
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_event(Event, State) ->
+    io:format("custom_error_report:handle_event - Event: ~p~n", [Event]),
+    {ok, State}.
+
+handle_call(_Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+handle_info(_Info, State) ->
+    {ok, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
--- src/sc_element_sup.erl
+++ src/sc_element_sup.erl
@@ -1,15 +1,16 @@
--module(sc_sup).
+-module(sc_element_sup).
 
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0,
+        start_child/2]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
-%-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 2000, Type, [I]}).
+-define(CHILD(I, Type), {I, {I, start_link, []}, temporary, brutal_kill, Type, [I]}).
 
 -define(SERVER, ?MODULE).
 
@@ -20,20 +21,17 @@
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
+start_child(Value, LeaseTime) ->
+    supervisor:start_child(?SERVER, [Value, LeaseTime]).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
-    ElementSup = {sc_element_sup, {sc_element_sup, start_link, []},
-		  permanent, 2000, supervisor, [sc_element]},
-
-    EventManager = {sc_event, {sc_event, start_link, []},
-		    permanent, 2000, worker, [sc_event]},
-
-    Children = [ElementSup, EventManager],
-    RestartStrategy = {one_for_one, 4, 3600},
+    Element = ?CHILD(sc_element, worker),
+    Children = [Element],
+    RestartStrategy = {simple_one_for_one, 0, 1},
     
     {ok, {RestartStrategy, Children}}.
 
